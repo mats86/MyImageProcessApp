c05d7564954faaa3e0b5c1fc7dec0b00c3a2858e
mats86 <maa86.ats@gmail.com>
2019-03-28 09:15:36 +0100

    Initial commit

---
+++ app/src/main/java/com/malattas/myimageprocessapp/WelcomeActivity.kt
@@ -0,0 +1,50 @@
+package com.malattas.myimageprocessapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welcome.*
+
+class WelcomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+
+//        btnColorTracking.setOnClickListener {
+//            val colorTrackingIntent = Intent(this@WelcomeActivity,
+//                TrackingColorActivity::class.java)
+//            startActivity(colorTrackingIntent)
+//        }
+//
+//        btnTextRecognition.setOnClickListener {
+//            val textRecognitionIntent = Intent(this@WelcomeActivity,
+//                TextRecognitionActivity::class.java)
+//            startActivity(textRecognitionIntent)
+//        }
+//
+//        btnColorization.setOnClickListener {
+//            val textRecognitionIntent = Intent(this@WelcomeActivity,
+//                ColorizationActivity::class.java)
+//            startActivity(textRecognitionIntent)
+//        }
+//
+//        btnGenderAge.setOnClickListener {
+//            val textRecognitionIntent = Intent(this@WelcomeActivity,
+//                GenderAgeActivity::class.java)
+//            startActivity(textRecognitionIntent)
+//        }
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    companion object {
+
+        // Used to load the 'native-lib' library on application startup.
+        init {
+            System.loadLibrary("native-lib")
+        }
+    }
+}
---
+++ textrecognition/src/main/java/com/malattas/textrecognition/TextRecognitionActivity.kt
@@ -0,0 +1,130 @@
+package com.malattas.textrecognition
+
+import android.content.Intent
+import android.graphics.Bitmap
+import android.media.MediaScannerConnection
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.util.*
+
+class TextRecognitionActivity : AppCompatActivity() {
+
+    private var btn: Button? = null
+    private var imageview: ImageView? = null
+    private val GALLERY = 1
+    private val CAMERA = 2
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_text_recognition)
+
+        btn = findViewById<View>(R.id.btnSelectImage) as Button
+        imageview = findViewById<View>(R.id.imageView) as ImageView
+
+        btn!!.setOnClickListener { showPictureDialog() }
+    }
+
+    private fun showPictureDialog() {
+        val pictureDialog = AlertDialog.Builder(this)
+        pictureDialog.setTitle("Select Action")
+        val pictureDialogItems = arrayOf("Select photo from gallery", "Capture photo from camera")
+        pictureDialog.setItems(pictureDialogItems) {
+                dialog, which ->
+            when (which) {
+                0 -> choosePhotoFromGallery()
+                1 -> takePhotoFromCamera()
+            }
+        }
+        pictureDialog.show()
+    }
+
+    private fun choosePhotoFromGallery() {
+        val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(galleryIntent, GALLERY)
+    }
+
+    private fun takePhotoFromCamera() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(intent, CAMERA)
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == GALLERY)
+        {
+            if (data != null)
+            {
+                val contentURI = data.data
+                try
+                {
+                    val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, contentURI)
+                    val path = saveImage(bitmap)
+
+
+                    Toast.makeText(this@TextRecognitionActivity, "Image saved! $path ", Toast.LENGTH_SHORT).show()
+                    imageview!!.setImageBitmap(bitmap)
+                }
+                catch (e: IOException) {
+                    e.printStackTrace()
+                    Toast.makeText(this@TextRecognitionActivity, "Failed!", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+        else if (requestCode == CAMERA)
+        {
+            val thumbnail = data!!.extras!!.get("data") as Bitmap
+            imageview!!.setImageBitmap(thumbnail)
+            saveImage(thumbnail)
+            Toast.makeText(this@TextRecognitionActivity, "Image saved!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun saveImage(myBitmap: Bitmap):String {
+        val bytes = ByteArrayOutputStream()
+        myBitmap.compress(Bitmap.CompressFormat.PNG, 90, bytes)
+        val wallpaperDirectory = File(
+            (Environment.getExternalStorageDirectory()).toString() + IMAGE_DIRECTORY)
+        // have the object build the directory structure, if needed.
+        Log.d("fee", wallpaperDirectory.toString())
+        if (!wallpaperDirectory.exists())
+        {
+            wallpaperDirectory.mkdirs()
+        }
+        try {
+            Log.d("heel",wallpaperDirectory.toString())
+            val f = File(wallpaperDirectory, ((Calendar.getInstance()
+                .timeInMillis).toString() + ".jpg"))
+            f.createNewFile()
+            val fo = FileOutputStream(f)
+            fo.write(bytes.toByteArray())
+            MediaScannerConnection.scanFile(this,
+                arrayOf(f.path),
+                arrayOf("image/jpeg"), null)
+            fo.close()
+            Log.d("TAG", "File Saved::--->" + f.absolutePath)
+
+            return f.absolutePath
+        }
+        catch (e1: IOException) {
+            e1.printStackTrace()
+        }
+
+        return ""
+    }
+
+    companion object {
+        private const val IMAGE_DIRECTORY = "/imageProcess"
+    }
+}
